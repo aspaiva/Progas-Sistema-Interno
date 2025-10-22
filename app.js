ce36794d9df16bfd60faba66ad8d78c4e3cac59e
Alexandre Paiva <aspaiva@gmail.com>
2025-10-21 21:56:53 -0300

    Ajuste das chamadas app.use

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const orcamentoRouter = require('./routes/orcamento');
 const authMiddleware = require('./middlewares/authMiddleware');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+const { MongoClient } = require('mongodb');
 
 const permissions = require('./middlewares/permissionMiddleware');
 
@@ -25,65 +26,75 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 authMiddleware(passport);
 
-MongoStore.create({
-  mongoUrl: process.env.DATABASE_CONNECTION,
-  dbName: 'sessions',
-  autoRemove: 'native',
-  ttl: 30 * 60
-}).then(store => {
+(async () => {
+  const client = await MongoClient.connect(process.env.DATABASE_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
   app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 30 * 60 * 1000 },
-    store
+    cookie: { maxAge: 30 * 60 * 1000 }, // 30 minutos
+    store: MongoStore.create({
+      client,
+      dbName: 'sessions',
+      autoRemove: 'native',
+      ttl: 30 * 60
+    })
   }));
-}).catch(err => {
-  console.error('Erro ao conectar ao MongoStore:', err);
-});
 
-// app.use(session({
-//   secret: process.env.SESSION_SECRET,
-//   resave: false,
-//   saveUninitialized: false,
-//   cookie: { maxAge: 30 * 60 * 1000 }, // 30 minutos
-//   store: MongoStore.create({
-//     mongoUrl: process.env.DATABASE_CONNECTION,
-//     dbName: 'sessions',
-//     autoRemove: 'native',
-//     ttl: 30 * 60
-//   })
-// }));
+  app.use(passport.session());
+  app.use(passport.initialize());
 
-app.use(passport.session());  
-app.use(passport.initialize());
 
-app.use('/', loginRouter);
-app.use('/login', loginRouter);
-app.use('/home', indexRouter);
-app.use('/users', permissions, usersRouter);
-app.use('/orcamento', permissions, orcamentoRouter);
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
+  // app.use(session({
+  //   secret: process.env.SESSION_SECRET,
+  //   resave: false,
+  //   saveUninitialized: false,
+  //   cookie: { maxAge: 30 * 60 * 1000 }, // 30 minutos
+  //   store: MongoStore.create({
+  //     mongoUrl: process.env.DATABASE_CONNECTION,
+  //     dbName: 'sessions',
+  //     autoRemove: 'native',
+  //     ttl: 30 * 60
+  //   })
+  // }));
 
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  app.use('/', loginRouter);
+  app.use('/login', loginRouter);
+  app.use('/home', indexRouter);
+  app.use('/users', permissions, usersRouter);
+  app.use('/orcamento', permissions, orcamentoRouter);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  // catch 404 and forward to error handler
+  app.use((req, res, next) => {
+    next(createError(404));
+  });
+
+  // error handler
+  app.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+  // Inicie seu servidor aqui
+  app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+  });
+})();
 
 module.exports = app;
