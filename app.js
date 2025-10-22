9c90eb6527e352a06d19c4d5c908b601fb23bc9d
Alexandre Paiva <aspaiva@gmail.com>
2025-10-22 15:28:20 -0300

    rotas não funcionando como esperado

--- app.js
+++ app.js
@@ -7,17 +7,18 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const passport = require('passport');
 
+//routes
 const loginRouter = require('./routes/login');
 const indexRouter = require('./routes/home');
 const usersRouter = require('./routes/users');
 const orcamentoRouter = require('./routes/orcamento');
 
 const authMiddleware = require('./middlewares/authMiddleware');
+const permissionMiddleware = require('./middlewares/permissionMiddleware');
+
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const { MongoClient } = require('mongodb');
-
-const permissions = require('./middlewares/permissionMiddleware');
 
 const app = express();
 
@@ -32,72 +33,49 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 authMiddleware(passport);
 
-(async () => {
-  const client = await MongoClient.connect(process.env.DATABASE_CONNECTION, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
-
-  app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: { maxAge: 30 * 60 * 1000 }, // 30 minutos
     store: MongoStore.create({
-      client,
+      mongoUrl: process.env.DATABASE_CONNECTION,
       dbName: 'sessions',
       autoRemove: 'native',
       ttl: 30 * 60
     })
-  }));
+  })
+);
 
-  app.use(passport.session());
-  app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.initialize());
 
+app.use('/', indexRouter);
+app.use('/home', indexRouter);
+app.use('/login', loginRouter);
+app.use('/users', permissionMiddleware, usersRouter);
+app.use('/orcamento', permissionMiddleware, orcamentoRouter);
 
+// Inicie seu servidor aqui
+app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
+  console.log('Servidor rodando na porta 3000 via ipv4');
+});
 
-  // app.use(session({
-  //   secret: process.env.SESSION_SECRET,
-  //   resave: false,
-  //   saveUninitialized: false,
-  //   cookie: { maxAge: 30 * 60 * 1000 }, // 30 minutos
-  //   store: MongoStore.create({
-  //     mongoUrl: process.env.DATABASE_CONNECTION,
-  //     dbName: 'sessions',
-  //     autoRemove: 'native',
-  //     ttl: 30 * 60
-  //   })
-  // }));
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
 
-  app.get('/', (req, res) => {
-    res.send('Servidor Express está funcionando!');
-  });
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  app.use('/login', loginRouter);
-  app.use('/home', indexRouter);
-  app.use('/users', permissions, usersRouter);
-  app.use('/orcamento', permissions, orcamentoRouter);
-
-  // catch 404 and forward to error handler
-  app.use((req, res, next) => {
-    next(createError(404));
-  });
-
-  // error handler
-  app.use((err, req, res, next) => {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
-
-  // Inicie seu servidor aqui
-  app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
-    console.log('Servidor rodando na porta 3000 via ipv4');
-  });
-})();
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
